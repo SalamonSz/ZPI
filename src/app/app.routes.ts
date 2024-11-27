14f9ac75519cc403e4c77c94b3f4101ab6ecae3c
SzymonSalamon <92257198+SzymonSalamon@users.noreply.github.com>
2024-11-27 21:53:50 +0100

    toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { OverviewComponent } from '../components/overview/overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: OverviewComponent }, /*
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: '' } // Przekierowanie dla nieistniejących tras*/
+  ];
