a7e78872934cb54ea62d656cff57908c94a07312
SzymonSalamon <92257198+SzymonSalamon@users.noreply.github.com>
2024-11-27 22:55:41 +0100

    dataStats

--- src/components/stats-card/stats-card.component.ts
+++ src/components/stats-card/stats-card.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { FormatValuePipe } from '../../pipes/value.pipe';
 
 @Component({
   selector: 'app-stats-card',
   standalone: true,
-  imports: [MatCardModule,MatIconModule, CommonModule],
+  imports: [MatCardModule,MatIconModule, CommonModule, FormatValuePipe],
   templateUrl: './stats-card.component.html',
   styleUrl: './stats-card.component.scss'
 })
--- src/pipes/value.pipe.ts
+++ src/pipes/value.pipe.ts
@@ -1,13 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'value',
+  name: 'formatValue',
   standalone: true
 })
-export class ValuePipe implements PipeTransform {
+export class FormatValuePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number | string, showNegativeSign: boolean = true): string {
+    if (typeof value === 'string') {
+      value = parseFloat(value.replace(',', '.'));
+    }
+
+    if (isNaN(value)) {
+      return '';
+    }
+    
+    let absValue = Math.abs(value);
+    let formattedValue = absValue.toLocaleString('pl-PL', {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2,
+    });
+
+    if (!showNegativeSign && value < 0) {
+      return formattedValue;
+    }
+
+    return value < 0 ? `-${formattedValue}` : formattedValue;
   }
-
 }
