6e773a94f8c15d926802ab7dd73d8fd634d04547
SzymonSalamon <92257198+SzymonSalamon@users.noreply.github.com>
2024-11-28 23:15:49 +0100

    add image

--- src/components/upload/upload.component.ts
+++ src/components/upload/upload.component.ts
@@ -1,3 +1,5 @@
+import { trigger, transition, style, animate } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -8,9 +10,20 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   imports: [ MatButtonModule,
     MatCardModule,
-    MatIconModule],
+    MatIconModule, CommonModule],
   templateUrl: './upload.component.html',
-  styleUrl: './upload.component.scss'
+  styleUrl: './upload.component.scss',
+  animations: [
+    trigger('fadeAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class UploadComponent {
 
@@ -19,5 +32,20 @@ export class UploadComponent {
   onInstructionClick() {
     this.instructionSignal.set(true);
   }
+  imageSrc: string | null = null;
 
+  onFileSelected(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+
+    if (fileInput.files && fileInput.files.length > 0) {
+      const file = fileInput.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.imageSrc = e.target.result; 
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
 }
